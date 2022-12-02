6bf461c6a701367bfacffb7ac4e39cf064cd020e
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-12-01 19:37:22 -0500

    init

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Timothy Manwell All rights reserved
 //
 // Created by: Timothy Manwell
-// Created on: Oct 2022
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -18,6 +18,27 @@ if (navigator.serviceWorker) {
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+ function myButtonClicked() {
+  const TAX = 1.13
+  const message = 3.99
+  const delivery = 15
+  const size = document.getElementById("size").value
+  const flavour = document.getElementById("flavour").value
+  const message = document.getElementById("message").value
+  const pickUpOrDelivery = document.getElementById("pick-up-or-delivery").value
+
+  const basePrice = (size + falvour) * TAX
+  const messageOnly = (size + flavour + message) * TAX
+  const deliveryOnly = (size + flavour + delivery) * TAX
+  const messageAndDelivery = (size + flavour + message + delivery) * TAX
+
+  if (message == "yes" && pickUpOrDelivery == "delivery"){
+    document.getElementById("total").innerHTML = "Your total is: $" + messageAndDelivery + "." 
+  } else if (message == "no" && pickUpOrDelivery == "delivery"){
+    document.getElementById("total").innerHTML = "Your total is: $" + deliveryOnly + "."
+  } else if (message == "yes" && pickUpOrDelivery == "pick"){
+    document.getElementById("total").innerHTML = "Your total is: $" + messageOnly + "."
+  } else{
+    document.getElementById("total").innerHTML = "Your total is: $" + basePrice + "."
+  }
 }
