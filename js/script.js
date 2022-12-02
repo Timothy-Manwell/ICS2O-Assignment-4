65dbe0ba08f3deea23b7e8b14730378d285e72ab
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-12-01 19:47:03 -0500

    test

--- js/script.js
+++ js/script.js
@@ -18,11 +18,13 @@ if (navigator.serviceWorker) {
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
+ function myButtonClicked() {
   const TAX = 1.13
+  const message = 3.99
+  const delivery = 15
   const size = document.getElementById("size").value
   const flavour = document.getElementById("flavour").value
-  const message = document.getElementById("message").value
+  const messageAnswer = document.getElementById("message").value
   const pickUpOrDelivery = document.getElementById("pick-up-or-delivery").value
 
   const basePrice = (size + falvour) * TAX
@@ -30,17 +32,13 @@ function myButtonClicked() {
   const deliveryOnly = (size + flavour + delivery) * TAX
   const messageAndDelivery = (size + flavour + message + delivery) * TAX
 
-  if (message == "yes" && pickUpOrDelivery == "delivery") {
-    document.getElementById("total").innerHTML =
-      "Your total is: $" + messageAndDelivery + "."
-  } else if (message == "no" && pickUpOrDelivery == "delivery") {
-    document.getElementById("total").innerHTML =
-      "Your total is: $" + deliveryOnly + "."
-  } else if (message == "yes" && pickUpOrDelivery == "pick") {
-    document.getElementById("total").innerHTML =
-      "Your total is: $" + messageOnly + "."
-  } else {
-    document.getElementById("total").innerHTML =
-      "Your total is: $" + basePrice + "."
+  if (messmessageAnswerage == "yes" && pickUpOrDelivery == "delivery"){
+    document.getElementById("total").innerHTML = "Your total is: $" + messageAndDelivery + "." 
+  } else if (messageAnswer == "no" && pickUpOrDelivery == "delivery"){
+    document.getElementById("total").innerHTML = "Your total is: $" + deliveryOnly + "."
+  } else if (messageAnswer == "yes" && pickUpOrDelivery == "pick"){
+    document.getElementById("total").innerHTML = "Your total is: $" + messageOnly + "."
+  } else{
+    document.getElementById("total").innerHTML = "Your total is: $" + basePrice + "."
   }
 }
